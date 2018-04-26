1a37019fb6a9f55aba324a2892155b0c0ed5b263
Vlady Veselinov <vladkodmc@gmail.com>
2018-04-26 17:27:12 +0100

    Fix shape of data

--- src/mnist.py
+++ src/mnist.py
@@ -5,42 +5,45 @@ from tflearn.layers.estimator import regression
 import tflearn.datasets.mnist as mnist
 
 X, Y, test_x, test_y = mnist.load_data(one_hot=True)
-print(X.shape, Y.shape, test_x.shape, test_y.shape)
+# print(Y.shape, Y.shape, test_x.shape, test_y.shape)
 
-print(X[0])
+# print(X)
+# print(type(X))
+print(Y[0])
+
 X = X.reshape([-1, 28, 28, 1])
 test_x = test_x.reshape([-1, 28, 28, 1])
 
-print(type(X), type(Y), type(test_x), type(test_y))
+# print(type(X), type(Y), type(test_x), type(test_y))
 print(X.shape, Y.shape, test_x.shape, test_y.shape)
 
-# conv_net = input_data(shape=[None, 28, 28, 1], name='input')
+conv_net = input_data(shape=[None, 28, 28, 1], name='input')
 
-# conv_net = conv_2d(conv_net, 32, 2, activation='relu')
-# conv_net = max_pool_2d(conv_net, 2)
+conv_net = conv_2d(conv_net, 32, 2, activation='relu')
+conv_net = max_pool_2d(conv_net, 2)
 
-# conv_net = conv_2d(conv_net, 64, 2, activation='relu')
-# conv_net = max_pool_2d(conv_net, 2)
+conv_net = conv_2d(conv_net, 64, 2, activation='relu')
+conv_net = max_pool_2d(conv_net, 2)
 
-# conv_net = fully_connected(conv_net, 1024, activation='relu')
-# conv_net = dropout(conv_net, 0.8)
+conv_net = fully_connected(conv_net, 1024, activation='relu')
+conv_net = dropout(conv_net, 0.8)
 
-# conv_net = fully_connected(conv_net, 10, activation='softmax')
-# conv_net = regression(conv_net, learning_rate=0.01, loss='categorical_crossentropy', name='targets')
+conv_net = fully_connected(conv_net, 10, activation='softmax')
+conv_net = regression(conv_net, learning_rate=0.01, loss='categorical_crossentropy', name='targets')
 
-# model = tflearn.DNN(conv_net)
+model = tflearn.DNN(conv_net)
 
-# # model.fit(
-# #     { 'input': X },
-# #     { 'targets': Y },
-# #     n_epoch=10,
-# #     validation_set=({ 'input': test_x }, { 'targets': test_y }),
-# #     snapshot_step=500,
-# #     show_metric=True,
-# #     run_id='mnist'
-# # )
-# # model.save('tflearncnn.model')
+# # # model.fit(
+# # #     { 'input': X },
+# # #     { 'targets': Y },
+# # #     n_epoch=10,
+# # #     validation_set=({ 'input': test_x }, { 'targets': test_y }),
+# # #     snapshot_step=500,
+# # #     show_metric=True,
+# # #     run_id='mnist'
+# # # )
+# # # model.save('tflearncnn.model')
 
-# model.load('tflearncnn.model')
+# # model.load('tflearncnn.model')
 
-# print(model.predict([test_x[1]]))
+# # print(model.predict([test_x[1]]))
--- src/test.py
+++ src/test.py
@@ -10,7 +10,7 @@ import numpy as np
 import os
 
 data_path = os.path.dirname(os.path.realpath(__file__)) + '/../data/data.csv'
-data = pd.read_csv(data_path, nrows=10)
+data = pd.read_csv(data_path)
 
 # Parse strings and make arrays the same shape
 data['lowLevel.mfcc'] = data['lowLevel.mfcc'].map(literal_eval)
@@ -26,36 +26,49 @@ data['category'] = data['category'].map(categories.index)
 train = data.sample(frac=0.8, random_state=200)
 test = data.drop(train.index)
 
-train_x = train['lowLevel.mfcc'].as_matrix()
-test_x = test['lowLevel.mfcc'].as_matrix()
+train_x = np.empty((train['lowLevel.mfcc'].size, 1300, 13))
+test_x = np.empty((test['lowLevel.mfcc'].size, 1300, 13))
+
+for index, item in enumerate(train['lowLevel.mfcc']):
+    train_x[index] = item
+
+for index, item in enumerate(test['lowLevel.mfcc']):
+    test_x[index] = item
 
 train_y = to_categorical(train['category'], len(categories))
 test_y = to_categorical(test['category'], len(categories))
 
-
-
-print(train_x.shape, train_y.shape, test_x.shape, test_y.shape)
-print(type(train_x), type(train_y), type(test_x), type(test_y))
-
 train_x = train_x.reshape([-1, 1300, 13, 1])
 test_x = test_x.reshape([-1, 1300, 13, 1])
 
-# net = input_data(shape=[None, 1300, 13, 1], name='input')
-# net = conv_2d(net, 32, 2, activation='relu')
-# net = tflearn.fully_connected(net, 32)
-# net = tflearn.fully_connected(net, 32)
-# net = tflearn.fully_connected(net, 2, activation='softmax')
-# net = tflearn.regression(net)
 
-# # Define model
-# model = tflearn.DNN(net)
+print(train_y)
 
-# # Train using gradient descent
-# model.fit(
-#     train_x,
-#     train_y,
-#     n_epoch=10,
-#     validation_set=(test_x, test_y),
-#     show_metric=True,
-#     run_id='genres'
-# )
+print(train_x.shape, train_y.shape, test_x.shape, test_y.shape)
+# print(type(train_x), type(train_y), type(test_x), type(test_y))
+
+net = input_data(shape=[None, 1300, 13, 1], name='input')
+net = conv_2d(net, 32, 2, activation='relu')
+net = max_pool_2d(net, 2)
+
+net = conv_2d(net, 64, 2, activation='relu')
+net = max_pool_2d(net, 2)
+
+net = fully_connected(net, 1024, activation='relu')
+net = dropout(net, 0.8)
+net = tflearn.fully_connected(net, len(categories), activation='softmax')
+net = regression(net, learning_rate=0.01, loss='categorical_crossentropy', name='targets')
+
+# Define model
+model = tflearn.DNN(net)
+
+# Train using gradient descent
+model.fit(
+    { 'input': train_x },
+    { 'targets': train_y },
+    n_epoch=10,
+    validation_set=({ 'input': test_x }, { 'targets': test_y }),
+    snapshot_step=500,
+    show_metric=True,
+    run_id='genres'
+)
